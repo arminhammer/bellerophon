70e6ae9e41fb2efa0dee85e182c34aa077ab093f
arminhammer <arminhammer@gmail.com>
2015-11-18 23:20:11 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+
+// report crashes to the Electron project
+require('crash-reporter').start();
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+function createMainWindow() {
+	const win = new electron.BrowserWindow({
+		width: 600,
+		height: 400
+	});
+
+	win.loadURL(`file://${__dirname}/index.html`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate-with-no-open-windows', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+});
