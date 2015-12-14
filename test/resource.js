f1155162e518dfa5b6de1073e8870e8895766b8b
arminhammer <arminhammer@gmail.com>
2015-12-13 22:31:03 -0500

    Adding unit tests for resource ASG, with some refactoring.

--- test/resource.js
+++ test/resource.js
@@ -8,115 +8,74 @@ var _ = require('lodash');
 //AWS.config.region = 'us-east-1';
 //var ASG = P.promisifyAll(new AWS.AutoScaling());
 
+function testCall(res, cb) {
+	return res
+		.call
+		.then(function(data) {
+			assert(data.ResponseMetadata.RequestId);
+			cb();
+		})
+		.catch(function(e) {
+			assert.fail(e);
+			cb();
+		});
+};
+
+function testConstructor(res) {
+	var name = 'test-001';
+	var resource = new res.construct(name, {});
+	assert.equal(resource.inTemplate, false);
+	assert(_.isEqual(resource.templateParams, {}));
+	assert.equal(resource.id, name);
+	assert.equal(resource.name, 'test001Resource');
+	assert(_.isEqual(resource.body, {}));
+	assert(resource.block);
+}
+
 describe('resource', function () {
 	it('buildName should return a unique alphanumeric name', function () {
 		var vpc = new Resource.EC2.VPC.construct('resource-001', {});
-		//var testName = 'resource-001';
 		assert.equal(vpc.name, 'resource001Resource');
 	});
 
-	it('AWS::AutoScaling::AutoScaling\'s call method should work', function(done) {
+	describe('AWS::AutoScaling::AutoScaling', function() {
+		it('AWS::AutoScaling::AutoScaling\'s call method should work', function(done) {
+			return testCall(Resource.AutoScaling.AutoScalingGroup, done);
+		});
 
-		return Resource.AutoScaling.AutoScalingGroup
-			.call
-			.then(function(data) {
-				assert(data.ResponseMetadata.RequestId);
-				done();
-			})
-			.catch(function(e) {
-				assert.fail(e);
-				done();
-			});
+		it('AWS::AutoScaling::AutoScaling\'s constructor method should work', function() {
+			testConstructor(Resource.AutoScaling.AutoScalingGroup);
+		});
 	});
 
-	it('AWS::AutoScaling::AutoScaling\'s constructor method should work', function() {
+	describe('AWS::AutoScaling::LaunchConfiguration', function() {
+		it('AWS::AutoScaling::LaunchConfiguration\'s call method should work', function(done) {
+			return testCall(Resource.AutoScaling.LaunchConfiguration, done);
+		});
 
-		var name = 'test-001';
-		var resource = new Resource.AutoScaling.AutoScalingGroup.construct(name, {});
-		assert.equal(resource.inTemplate, false);
-		assert(_.isEqual(resource.templateParams, {}));
-		assert.equal(resource.id, name);
-		assert.equal(resource.name, 'test001Resource');
-		assert(_.isEqual(resource.body, {}));
-		assert(resource.block);
+		it('AWS::AutoScaling::AutoScaling\'s constructor method should work', function() {
+			testConstructor(Resource.AutoScaling.LaunchConfiguration);
+		});
 	});
 
-	it('AWS::AutoScaling::LaunchConfiguration\'s call method should work', function(done) {
+	describe('AWS::AutoScaling::ScalingPolicy', function() {
+		it('AWS::AutoScaling::ScalingPolicy\'s call method should work', function(done) {
+			return testCall(Resource.AutoScaling.ScalingPolicy, done);
+		});
 
-		return Resource.AutoScaling.LaunchConfiguration
-			.call
-			.then(function(data) {
-				assert(data.ResponseMetadata.RequestId);
-				done();
-			})
-			.catch(function(e) {
-				assert.fail(e);
-				done();
-			});
+		it('AWS::AutoScaling::ScalingPolicy\'s constructor method should work', function() {
+			testConstructor(Resource.AutoScaling.ScalingPolicy);
+		});
 	});
 
-	it('AWS::AutoScaling::LaunchConfiguration\'s constructor method should work', function() {
+	describe('AWS::AutoScaling::ScheduledAction', function() {
+		it('AWS::AutoScaling::ScheduledAction\'s call method should work', function(done) {
+			return testCall(Resource.AutoScaling.ScheduledAction, done);
+		});
 
-		var name = 'test-001';
-		var resource = new Resource.AutoScaling.LaunchConfiguration.construct(name, {});
-		assert.equal(resource.inTemplate, false);
-		assert(_.isEqual(resource.templateParams, {}));
-		assert.equal(resource.id, name);
-		assert.equal(resource.name, 'test001Resource');
-		assert(_.isEqual(resource.body, {}));
-		assert(resource.block);
-	});
-
-	it('AWS::AutoScaling::ScalingPolicy\'s call method should work', function(done) {
-
-		return Resource.AutoScaling.ScalingPolicy
-			.call
-			.then(function(data) {
-				assert(data.ResponseMetadata.RequestId);
-				done();
-			})
-			.catch(function(e) {
-				assert.fail(e);
-				done();
-			});
-	});
-
-	it('AWS::AutoScaling::ScalingPolicy\'s constructor method should work', function() {
-
-		var name = 'test-001';
-		var resource = new Resource.AutoScaling.ScalingPolicy.construct(name, {});
-		assert.equal(resource.inTemplate, false);
-		assert(_.isEqual(resource.templateParams, {}));
-		assert.equal(resource.id, name);
-		assert.equal(resource.name, 'test001Resource');
-		assert(_.isEqual(resource.body, {}));
-		assert(resource.block);
-	});
-
-	it('AWS::AutoScaling::ScheduledAction\'s call method should work', function(done) {
-
-		return Resource.AutoScaling.ScheduledAction
-			.call
-			.then(function(data) {
-				assert(data.ResponseMetadata.RequestId);
-				done();
-			})
-			.catch(function(e) {
-				assert.fail(e);
-				done();
-			});
-	});
-
-	it('AWS::AutoScaling::ScheduledAction\'s constructor method should work', function() {
-
-		var name = 'test-001';
-		var resource = new Resource.AutoScaling.ScheduledAction.construct(name, {});
-		assert.equal(resource.inTemplate, false);
-		assert(_.isEqual(resource.templateParams, {}));
-		assert.equal(resource.id, name);
-		assert.equal(resource.name, 'test001Resource');
-		assert(_.isEqual(resource.body, {}));
-		assert(resource.block);
+		it('AWS::AutoScaling::ScheduledAction\'s constructor method should work', function() {
+			testConstructor(Resource.AutoScaling.ScheduledAction);
+		});
 	});
 
 });
