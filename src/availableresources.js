c6ec8c27d5efe2ad2d5e15115b574b4080301f3e
arminhammer <arminhammer@gmail.com>
2016-01-16 09:10:48 -0500

    Streamlining availableresources

--- src/availableresources.js
+++ src/availableresources.js
@@ -22,13 +22,13 @@ var AvailableResources = {
 		CloudFormation : {
 			icon: 'ManagementTools_CloudFormation',
 			types: {
-				//AWS::CloudFormation::Authentication
-				//AWS::CloudFormation::CustomResource
-				//AWS::CloudFormation::Init
-				//AWS::CloudFormation::Interface
-				//AWS::CloudFormation::Stack
-				//AWS::CloudFormation::WaitCondition
-				//AWS::CloudFormation::WaitConditionHandle
+				Authentication: {},
+				CustomResource: {},
+				Init: {},
+				Interface: {},
+				Stack: {},
+				WaitCondition: {},
+				WaitConditionHandle: {}
 			}
 		},
 		CloudFront: {
@@ -40,55 +40,55 @@ var AvailableResources = {
 		CloudTrail: {
 			icon: 'ManagementTools_CloudTrail',
 			types: {
-				//AWS::CloudTrail::Trail
+				Trail: {}
 			}
 		},
 		CloudWatch: {
 			icon: 'ManagementTools_CloudWatch',
 			types: {
-				//AWS::CloudWatch::Alarm
+				Alarm: {}
 			}
 		},
 		CodeDeploy: {
 			icon: 'DeploymentManagement_CodeDeploy',
 			types: {
-				//AWS::CodeDeploy::Application
-				//AWS::CodeDeploy::DeploymentConfig
-				//AWS::CodeDeploy::DeploymentGroup
+				Application: {},
+				DeploymentConfig: {},
+				DeploymentGroup: {}
 			}
 		},
 		CodePipeline: {
 			icon: 'DeploymentManagement_CodePipeline',
 			types: {
-				//AWS::CodePipeline::CustomActionType
-				//AWS::CodePipeline::Pipeline
+				CustomActionType: {},
+				Pipeline: {}
 			}
 		},
 		Config: {
 			icon: 'ManagementTools_Config',
 			types: {
-				//AWS::Config::ConfigRule
-				//AWS::Config::ConfigurationRecorder
-				//AWS::Config::DeliveryChannel
+				ConfigRule: {},
+				ConfigurationRecorder: {},
+				DeliveryChannel: {}
 			}
 		},
 		DataPipeline: {
 			icon: 'Analytics_DataPipeline',
 			types: {
-				//AWS::DataPipeline::Pipeline
+				Pipeline: {}
 			}
 		},
 		DirectoryService: {
 			icon: 'SecurityIdentity_DirectoryService',
 			types: {
-				//AWS::DirectoryService::MicrosoftAD
-				//AWS::DirectoryService::SimpleAD
+				MicrosoftAD: {},
+				SimpleAD: {}
 			}
 		},
 		DynamoDB: {
 			icon: 'Database_DynamoDB',
 			types: {
-				//AWS::DynamoDB::Table
+				Table: {}
 			}
 		},
 		EC2: {
@@ -130,128 +130,128 @@ var AvailableResources = {
 		ECS: {
 			icon: 'Compute_AmazonEC2ContainerService',
 			types: {
-				//AWS::ECS::Cluster
-				//AWS::ECS::Service
-				//AWS::ECS::TaskDefinition
+				Cluster: {},
+				Service: {},
+				TaskDefinition: {}
 			}
 		},
 		EFS: {
 			icon: 'StorageContentDelivery_EFS',
 			types: {
-				//AWS::EFS::FileSystem
-				//AWS::EFS::MountTarget
+				FileSystem: {},
+				MountTarget: {}
 			}
 		},
 		ElastiCache: {
 			icon: 'Database_AmazonElasticCache',
 			types: {
-				//AWS::ElastiCache::CacheCluster
-				//AWS::ElastiCache::ParameterGroup
-				//AWS::ElastiCache::ReplicationGroup
-				//AWS::ElastiCache::SecurityGroup
-				//AWS::ElastiCache::SecurityGroupIngress
-				//AWS::ElastiCache::SubnetGroup
+				CacheCluster: {},
+				ParameterGroup: {},
+				ReplicationGroup: {},
+				SecurityGroup: {},
+				SecurityGroupIngress: {},
+				SubnetGroup: {}
 			}
 		},
 		ElasticBeanstalk: {
 			icon: 'Compute_Elastic Beanstalk',
 			types: {
-				//AWS::ElasticBeanstalk::Application
-				//AWS::ElasticBeanstalk::ApplicationVersion
-				//AWS::ElasticBeanstalk::ConfigurationTemplate
-				//AWS::ElasticBeanstalk::Environment
+				Application: {},
+				ApplicationVersion: {},
+				ConfigurationTemplate: {},
+				Environment: {}
 			}
 		},
 		ElasticLoadBalancing: {
 			icon: 'Compute_ElasticLoadBalancing',
 			types: {
-				//AWS::ElasticLoadBalancing::LoadBalancer
+				LoadBalancer: {}
 			}
 		},
 		IAM: {
 			icon: 'SecurityIdentity_IdentityAccessManagement',
 			types: {
-				//AWS::IAM::AccessKey
-				//AWS::IAM::Group
-				//AWS::IAM::InstanceProfile
-				//AWS::IAM::ManagedPolicy
-				//AWS::IAM::Policy
-				//AWS::IAM::Role
-				//AWS::IAM::User
-				//AWS::IAM::UserToGroupAddition
+				AccessKey: {},
+				Group: {},
+				InstanceProfile: {},
+				ManagedPolicy: {},
+				Policy: {},
+				Role: {},
+				User: {},
+				UserToGroupAddition: {}
 			}
 		},
 		Kinesis: {
 			icon: 'Analytics_Kinesis',
 			types: {
-				//AWS::Kinesis::Stream
+				Stream: {}
 			}
 		},
 		KMS: {
 			icon: 'SecurityIdentity_KeyManagementService',
 			types: {
-				//AWS::KMS::Key
+				Key: {}
 			}
 		},
 		Lambda: {
 			icon: 'Compute_Lambda',
 			types: {
-				//AWS::Lambda::EventSourceMapping
-				//AWS::Lambda::Function
-				//AWS::Lambda::Permission
+				EventSourceMapping: {},
+				Function: {},
+				Permission: {}
 			}
 		},
 		Logs: {
 			icon: 'Compute_AmazonEC2_CloudWatch',
 			types: {
-				//AWS::Logs::Destination
-				//AWS::Logs::LogGroup
-				//AWS::Logs::LogStream
-				//AWS::Logs::MetricFilter
-				//AWS::Logs::SubscriptionFilter
+				Destination: {},
+				LogGroup: {},
+				LogStream: {},
+				MetricFilter: {},
+				SubscriptionFilter: {},
 			}
 		},
 		OpsWorks: {
 			icon: 'ManagementTools_OpsWorks',
 			types: {
-				//AWS::OpsWorks::App
-				//AWS::OpsWorks::ElasticLoadBalancerAttachment
-				//AWS::OpsWorks::Instance
-				//AWS::OpsWorks::Layer
-				//AWS::OpsWorks::Stack
+				App: {},
+				ElasticLoadBalancerAttachment: {},
+				Instance: {},
+				Layer: {},
+				Stack: {}
 			}
 		},
 		RDS: {
 			icon: 'Database_AmazonRDS',
 			types: {
-				//AWS::RDS::DBCluster
-				//AWS::RDS::DBClusterParameterGroup
-				//AWS::RDS::DBInstance
-				//AWS::RDS::DBParameterGroup
-				//AWS::RDS::DBSecurityGroup
-				//AWS::RDS::DBSecurityGroupIngress
-				//AWS::RDS::DBSubnetGroup
-				//AWS::RDS::EventSubscription
-				//AWS::RDS::OptionGroup
+				DBCluster: {},
+				DBClusterParameterGroup: {},
+				DBInstance: {},
+				DBParameterGroup: {},
+				DBSecurityGroup: {},
+				DBSecurityGroupIngress: {},
+				DBSubnetGroup: {},
+				EventSubscription: {},
+				OptionGroup: {}
 			}
 		},
 		Redshift: {
 			icon: 'Database_AmazonRedShift',
 			types: {
-				//AWS::Redshift::Cluster
-				//AWS::Redshift::ClusterParameterGroup
-				//AWS::Redshift::ClusterSecurityGroup
-				//AWS::Redshift::ClusterSecurityGroupIngress
-				//AWS::Redshift::ClusterSubnetGroup
+				Cluster: {},
+				ClusterParameterGroup: {},
+				ClusterSecurityGroup: {},
+				ClusterSecurityGroupIngress: {},
+				ClusterSubnetGroup: {}
 			}
 		},
 		Route53: {
 			icon: 'Networking_Route53',
 			types: {
-				//AWS::Route53::HealthCheck
-				//AWS::Route53::HostedZone
-				//AWS::Route53::RecordSet
-				//AWS::Route53::RecordSetGroup
+				HealthCheck: {},
+				HostedZone: {},
+				RecordSet: {},
+				RecordSetGroup: {}
 			}
 		},
 		S3: {
@@ -264,43 +264,43 @@ var AvailableResources = {
 		SDB: {
 			icon: '',
 			types: {
-				//AWS::SDB::Domain
+				Domain: {}
 			}
 		},
 		SNS: {
 			icon: 'MobileServices_SNS',
 			types: {
-				//AWS::SNS::Topic
-				//AWS::SNS::TopicPolicy
+				Topic: {},
+				TopicPolicy: {}
 			}
 		},
 		SQS: {
 			icon: 'ApplicationServices_AmazonSQS',
 			types: {
-				//AWS::SQS::Queue
-				//AWS::SQS::QueuePolicy
+				Queue: {},
+				QueuePolicy: {}
 			}
 		},
 		SSM: {
 			icon: '',
 			types: {
-				//AWS::SSM::Document
+				Document: {}
 			}
 		},
 		WAF: {
 			icon: '',
 			types: {
-				//AWS::WAF::ByteMatchSet
-				//AWS::WAF::IPSet
-				//AWS::WAF::Rule
-				//AWS::WAF::SqlInjectionMatchSet
-				//AWS::WAF::WebACL
+				ByteMatchSet: {},
+				IPSet: {},
+				Rule: {},
+				SqlInjectionMatchSet: {},
+				WebACL: {}
 			}
 		},
 		WorkSpaces: {
 			icon: 'AmazonWorkSpaces',
 			types: {
-				//AWS::WorkSpaces::Workspace
+				Workspace: {}
 			}
 		}
 	},
